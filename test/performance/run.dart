771a988c80f43d06dfdcf59e17d767e13f5197f8
Daniel Robinson <daniel.robinson.open@gmail.com>
2014-11-04 10:58:41 +0000

    add more performance tests to compare Emitters/Streamers/Events Emitters: 0.7s Streamers: 8.6s Events: 27.1s

--- test/performance/run.dart
+++ test/performance/run.dart
@@ -1,4 +1,6 @@
-import 'package:streamers/streamers.dart';
+import 'package:emitters/emitters.dart' as em;
+import 'package:streamers/streamers.dart' as st;
+import 'package:events/events.dart' as ev;
 import 'dart:async';
 
 abstract class TestInterface{int val;}
@@ -38,30 +40,92 @@ void main() {
    */
   int numOfObjToEmit = 100000;
   int numOfDumbSubs = 200;
-  Function emitObj = () => new Z();
+  Function emitObj = () => new A();
   Type typeToListenTo = A;
   /**
    * End of config variables.
    */
 
-  executePerformanceTest(numOfObjToEmit, numOfDumbSubs, emitObj, typeToListenTo);
+  executeEmittersPerformanceTest(numOfObjToEmit, numOfDumbSubs, emitObj, typeToListenTo)
+  .then((_){
+    executeStreamersPerformanceTest(numOfObjToEmit, numOfDumbSubs, emitObj, typeToListenTo)
+    .then((_){
+      executeEventsPerformanceTest(numOfObjToEmit, numOfDumbSubs, emitObj, typeToListenTo);
+    });
+  });
 }
 
-Future executePerformanceTest(int numOfObjToEmit, int numOfDumbSubs, Function emitObj, Type typeToListenTo){
+Future executeEmittersPerformanceTest(int numOfObjToEmit, int numOfDumbSubs, Function emitObj, Type typeToListenTo){
   Completer completer = new Completer();
   Stopwatch stopwatch = new Stopwatch();
-  Emitter emitter = new Emitter();
+  em.Emitter emitter = new em.Emitter();
   var dumbHandler = (obj){};
   var actualHandler = (obj){
-    if(obj.val == numOfObjToEmit - 1){
+    if(obj.data.val == numOfObjToEmit - 1){
       stopwatch.stop();
-      print('Emitting $numOfObjToEmit ${emitObj().runtimeType} whilst listening to $typeToListenTo, by $numOfDumbSubs handlers took: ${stopwatch.elapsed}');
+      print('Emitters lib: Emitting $numOfObjToEmit ${emitObj().runtimeType} whilst listening to $typeToListenTo, by $numOfDumbSubs handlers took: ${stopwatch.elapsed}');
       if(!completer.isCompleted) completer.complete();
     }
   };
 
   // subscribe handlers
-  emitter.on(All).listen(actualHandler);
+  emitter.on(em.All, actualHandler);
+  for(var i = 0; i < numOfDumbSubs; i++){
+    emitter.on(typeToListenTo, dumbHandler);
+  }
+
+  // emit objects and start timing
+  stopwatch.start();
+  for(var i = 0; i < numOfObjToEmit; i++){
+    emitter.emit(emitObj()..val = i);
+  }
+
+  return completer.future;
+}
+
+Future executeStreamersPerformanceTest(int numOfObjToEmit, int numOfDumbSubs, Function emitObj, Type typeToListenTo){
+  Completer completer = new Completer();
+  Stopwatch stopwatch = new Stopwatch();
+  st.Emitter emitter = new st.Emitter();
+  var dumbHandler = (obj){};
+  var actualHandler = (obj){
+    if(obj.val == numOfObjToEmit - 1){
+      stopwatch.stop();
+      print('Streamers lib: Emitting $numOfObjToEmit ${emitObj().runtimeType} whilst listening to $typeToListenTo, by $numOfDumbSubs handlers took: ${stopwatch.elapsed}');
+      if(!completer.isCompleted) completer.complete();
+    }
+  };
+
+  // subscribe handlers
+  emitter.on(st.All).listen(actualHandler);
+  for(var i = 0; i < numOfDumbSubs; i++){
+    emitter.on(typeToListenTo).listen(dumbHandler);
+  }
+
+  // emit objects and start timing
+  stopwatch.start();
+  for(var i = 0; i < numOfObjToEmit; i++){
+    emitter.emit(emitObj()..val = i);
+  }
+
+  return completer.future;
+}
+
+Future executeEventsPerformanceTest(int numOfObjToEmit, int numOfDumbSubs, Function emitObj, Type typeToListenTo){
+  Completer completer = new Completer();
+  Stopwatch stopwatch = new Stopwatch();
+  ev.Events emitter = new ev.Events();
+  var dumbHandler = (obj){};
+  var actualHandler = (obj){
+    if(obj.val == numOfObjToEmit - 1){
+      stopwatch.stop();
+      print('Events lib: Emitting $numOfObjToEmit ${emitObj().runtimeType} whilst listening to $typeToListenTo, by $numOfDumbSubs handlers took: ${stopwatch.elapsed}');
+      if(!completer.isCompleted) completer.complete();
+    }
+  };
+
+  // subscribe handlers
+  emitter.on(null).listen(actualHandler);
   for(var i = 0; i < numOfDumbSubs; i++){
     emitter.on(typeToListenTo).listen(dumbHandler);
   }
